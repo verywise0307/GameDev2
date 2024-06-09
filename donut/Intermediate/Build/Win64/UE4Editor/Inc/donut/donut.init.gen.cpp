f9fb7383d35e86b7c2f04bc34859d3331dc6f76f
pinkypie2128 <pinkypie2128@naver.com>
2024-06-09 13:53:30 +0900

    Revert "."
    This reverts commit 3b79a3d49df685cc057835d443988b80e2732946.

--- donut/Intermediate/Build/Win64/UE4Editor/Inc/donut/donut.init.gen.cpp
+++ donut/Intermediate/Build/Win64/UE4Editor/Inc/donut/donut.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodedonut_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x93C99A1A,
-				0x2BDE86BD,
+				0x5DC29ABC,
+				0x62F4E4C5,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
